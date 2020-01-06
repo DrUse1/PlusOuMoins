60988d7deed881607b4025d7bdc3e8c81e3ea8f8
DrUse1 <47464474+DrUse1@users.noreply.github.com>
2020-01-06 14:50:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var ntf = Math.floor(Math.random() * 1001);
+var ntf = Math.floor(Math.random() * 101);
 
 function submit() {
   if (document.getElementById("submit_button").innerHTML == "Valider") {
